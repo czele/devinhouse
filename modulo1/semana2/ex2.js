b09c4e59876c2dd3bb0b3a842032d3834b2e7964
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-12-06 22:29:10 -0300

    Ex 2 com js separado

---
+++ modulo1/semana2/ex2.js
@@ -0,0 +1,10 @@
+var idade = window.prompt('Qual sua idade?');
+
+if (idade <= 15) {
+    'Você é considerado jovem'};
+if (idade > 15 && idade <= 64) {
+    'Você é considerado adulto'};
+if (idade > 64) {
+    'Você é considerado idoso'};
+
+window.alert();
