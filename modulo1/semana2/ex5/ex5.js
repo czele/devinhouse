5d0034946e57a4551a70f8f9070d9cef94e30a54
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-12-08 10:27:02 -0300

    ex6

--- modulo1/semana2/ex5/ex5.js
+++ modulo1/semana2/ex5/ex5.js
@@ -1,13 +1,17 @@
-var numeroden = window.prompt('Que número deseja verificar:');
+var numeroden = parseInt(window.prompt('Que número deseja verificar:'));
 parseInt(numeroden);
-var contadorpar = 0; 
-var contadorimpar = 0;
+var contadorpar = 0; var somapar = 0;
+var contadorimpar = 0; var somaimpar = 0;
 
 for (i=0; i<=numeroden; i++) {
     if(i%2==0) {
         contadorpar++;
+        somapar += contadorpar;
+        window.alert(`A quantidade de números pares é: ${contadorpar}`)
     }
     else{
         contadorimpar++;
+        somaimpar += contadorimpar;
+        window.alert(`A quantidade de números ímpares é: ${contadorimpar}`)
     } 
 }
---
+++ modulo1/semana2/ex6/ex6.js
@@ -0,0 +1,34 @@
+var operacao = window.prompt('Que operação deseja realizar?')
+var num1 = parseFloat(window.prompt('Qual o primeiro número para calcular:'))
+var num2 = parseFloat(window.prompt('Qual o segundo número para calcular:'))
+
+if (operacao == '+') {
+    window.alert(`O resultado da soma é: ${ressoma}`)
+}
+if (operacao == '-') {
+    window.alert(`O resultado da subtração é: ${num1 - num2}`)
+}
+if (operacao == '*') {
+    window.alert(`O resultado da multiplicação é: ${num1 * num2}`)
+}
+if (operacao == '/') {
+    window.alert(`O resultado da divisão é: ${num1 / num2}`)
+}var operacao = window.prompt('Que operação deseja realizar?')
+var num1 = parseFloat(window.prompt('Qual o primeiro número para calcular:'))
+var num2 = parseFloat(window.prompt('Qual o segundo número para calcular:'))
+
+if (operacao == '+') {
+    window.alert(`O resultado da soma é: ${ressoma}`)
+}
+if (operacao == '-') {
+    window.alert(`O resultado da subtração é: ${num1 - num2}`)
+}
+if (operacao == '*') {
+    window.alert(`O resultado da multiplicação é: ${num1 * num2}`)
+}
+if (operacao == '/') {
+    window.alert(`O resultado da divisão é: ${num1 / num2}`)
+}
+/*else {
+    operacao = window.prompt('Operação inválida, por favor, insira uma opção válida:')
+}*/
