9084d4aa5fe3a3f1350d9a6252ba0cdc1449dbab
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-12-08 13:55:36 -0300

    Enunciados

---
+++ modulo1/semana2/ex7/ex7.js
@@ -0,0 +1,9 @@
+var numprimo = 0
+
+for (i=0; i<=100; i++) {
+    if (numprimo%numprimo == 0) {
+        console.log(numprimo)
+    }
+    else {
+    }
+}
--- modulo1/semana2/ex8/ex8.js
+++ modulo1/semana2/ex8/ex8.js
@@ -2,5 +2,5 @@ var numinicial = parseInt(window.prompt('Com que número deseja iniciar a PA:'))
 var raiz = parseInt(window.prompt('Qual será a raiz da PA:'));
 
 for (i=0; i < 10; i++) {
-    console.log(numinicial += raiz)
+    window.alert(`${numinicial += raiz}`)
 }
