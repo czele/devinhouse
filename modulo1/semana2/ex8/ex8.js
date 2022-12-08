7247c40652dba6f6cccbe304337f357d9142d5c8
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-12-08 11:42:27 -0300

    ex8

---
+++ modulo1/semana2/ex8/ex8.js
@@ -0,0 +1,6 @@
+var numinicial = parseInt(window.prompt('Com que número deseja iniciar a PA:'));
+var raiz = parseInt(window.prompt('Qual será a raiz da PA:'));
+
+for (i=0; i < 10; i++) {
+    console.log(numinicial += raiz)
+}
