850c5af7fd717e632527381b18dac6824f308eae
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-12-15 15:49:39 -0300

    8 com erro

---
+++ modulo1/semana3/ex8/ex8.js
@@ -0,0 +1,7 @@
+uneDobraRetorna = (array1, ...numeros) => {
+    for(i=0; i< numeros.length; i++) {
+        parseInt(numeros *= 2)} 
+    return console.log(array1, numeros);
+}
+
+uneDobraRetorna([ 1, 2, 3], 3, 5, 6)
