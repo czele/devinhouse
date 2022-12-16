6111a0ed5c47d734b3a8a1547f0e2fe1d294b5dc
Camila Zelenovsky <camila.zelenovsky@cat.com>
2022-12-16 20:57:41 -0300

    ex6

--- modulo1/semana3/ex6/ex6.js
+++ modulo1/semana3/ex6/ex6.js
@@ -1,4 +1,19 @@
-let palindromo = (palavra) => { 
+let palindromo = (texto) => { 
+    const meio = texto.length / 2;
+    const fim = texto.length - 1;
+
+    for (let i = 0; i < meio; i++) {
+        if (texto[i] !== texto[fim - i]) {
+        return false;
+    }
+    }
+     return true;
 }
 
-console.log(palindromo(""))
+const teste1 = palindromo('osso');
+console.log(teste1)
+
+const testenome = palindromo('camila')
+console.log(testenome);
+
+const frase = palindromo('socorrame subi em um onibus em marrocos')
