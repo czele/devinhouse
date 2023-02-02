3074c48598af141901388ff73f4635faf09bae14
Camila Zelenovsky <camila.zelenovsky@cat.com>
2023-02-02 09:21:34 -0300

    ex7

---
+++ modulo1/semana5/ex7/Time.js
@@ -0,0 +1,11 @@
+class Time {
+    constructor (nome, sigla, vit, der, emp, golmarc, golsof) {
+        this.nome = nome;
+        this.sigla = sigla;
+        this.vit = vit;
+        this.der = der;
+        this.emp = emp;
+        this.golmarc = golmarc;
+        this.golsof = golsof;
+    }
+}
