623b1c708d62057769f648493d758c0125844630
Camila Zelenovsky <camila.zelenovsky@cat.com>
2023-02-08 18:35:33 -0300

    ex4

--- modulo1/semana6/ex4/app.js
+++ modulo1/semana6/ex4/app.js
@@ -1,5 +1,7 @@
 const fakeTime = () => parseInt(Math.random() * 10000);
-
 const retornoEmpresa1 = new Promise((resolve) => setTimeout(resolve, fakeTime(), 'Empresa 1'));
-
 const retornoEmpresa2 = new Promise((resolve) => setTimeout(resolve, fakeTime(), 'Empresa 2'));
+
+Promise.race([retornoEmpresa1, retornoEmpresa2])
+.then((valor) => {console.log(valor)})
+.catch((err) => console.log(err))
