8c6e45f2fa09eaa712b59afb1abd6b9a3b83a049
Camila Zelenovsky <camila.zelenovsky@cat.com>
2023-02-10 12:55:30 -0300

    ex7

--- modulo1/semana6/ex7/app.js
+++ modulo1/semana6/ex7/app.js
@@ -1,15 +1,17 @@
-const buscarPersonagens = async () => {
-    const baseUrl = await fetch ('https://rickandmortyapi.com/api/character', {
-        method: 'GET',
-    })
-    
-    const info = await baseUrl.json()
-  
-    console.log(info.count)
-    /*for (const i=0; i <= ids; i++) {
-        console.log(i)
-    }*/ 
+const BASE_URL = 'https://rickandmortyapi.com/api/character/'
 
+const buscarPersonagens = async () => {
+    const response = await fetch (BASE_URL)
+    
+    const info = await response.json()
+    const ids = info.info.count
+
+    //Coloquei o i até 3 pq senão ia fazer 800 requisições na API
+    for (let i=1; i <= 3/*ids*/; i++) {
+        const listaPersonagens = await fetch (`${BASE_URL}${i}`)
+        const responseListaPersonagens = await listaPersonagens.json()
+        console.log(`O nome dele é ${responseListaPersonagens.name} e a sua imagem é ${responseListaPersonagens.image}`)
+    }
 }
 
 buscarPersonagens()
