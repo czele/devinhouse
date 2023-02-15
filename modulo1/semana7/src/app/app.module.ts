3aab37d31ad264839f4aaec8340d1b4f5f91e555
Camila Zelenovsky <camila.zelenovsky@cat.com>
2023-02-15 11:58:07 -0300

    ex3

--- modulo1/semana7/src/app/app.module.ts
+++ modulo1/semana7/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarroselComponent } from './carrosel/carrosel.component';
 import { GaleriaComponent } from './components/galeria/galeria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarroselComponent,
     GaleriaComponent
   ],
   imports: [
