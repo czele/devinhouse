61bda7f2d01f71e5a55d2f19974484b27c190ed0
Camila Zelenovsky <camila.zelenovsky@cat.com>
2023-02-23 15:53:44 -0300

    httpclientmodule

--- modulo1/semana8/src/app/app.module.ts
+++ modulo1/semana8/src/app/app.module.ts
@@ -1,8 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { StarWarsComponent } from './components/star-wars/star-wars.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { StarWarsComponent } from './components/star-wars/star-wars.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
