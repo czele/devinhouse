b2f06d7db62eed9084f72d44a1322a6a97a3fdc0
Camila Zelenovsky <camila.zelenovsky@cat.com>
2023-02-24 13:42:37 -0300

    Service no HTML

--- modulo1/semana8/src/app/components/star-wars/star-wars.component.ts
+++ modulo1/semana8/src/app/components/star-wars/star-wars.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { StarWarsServiceService } from './../../star-wars-service.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-star-wars',
   templateUrl: './star-wars.component.html',
   styleUrls: ['./star-wars.component.css']
 })
-export class StarWarsComponent {
+export class StarWarsComponent implements OnInit {
 
+  public getInfoPersonagem1: string[] = [];
+  public getInfoPersonagem2: string[] = [];
+
+  constructor (private _service: StarWarsServiceService) {}
+
+  ngOnInit(): void {
+    this.getInfoServicePersonagem1();
+    this.getInfoServicePersonagem2();
+  }
+
+  public getInfoServicePersonagem1() {
+    return this.getInfoPersonagem1 = this._service.getPersonagem1();
+  }
+
+  public getInfoServicePersonagem2() {
+    return this.getInfoPersonagem2 = this._service.getPersonagem2();
+  }
 }
--- modulo1/semana8/src/app/star-wars-service.service.ts
+++ modulo1/semana8/src/app/star-wars-service.service.ts
@@ -5,5 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class StarWarsServiceService {
 
-  constructor() { }
+  public personagem1: string[] = ["teste", "camila", "teste"]
+  public personagem2: string[] = ["teste", "luquinhas", "teste"]
+
+  public getPersonagem1():string[]{
+    return this.personagem1;
+  }
+
+  public getPersonagem2():string[]{
+    return this.personagem2;
+  }
+
 }
