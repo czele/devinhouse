f4e90a1dc58c6f6c5bcfce116b31b57117ce6971
Camila Zelenovsky <camila_zele@hotmail.com>
2023-03-14 19:34:13 -0300

    authguard

--- modulo1/semana10/src/app/app-routing.module.ts
+++ modulo1/semana10/src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { ContaComponent } from './components/conta/conta.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { GuardaHomeGuard } from './guarda-home.guard';
 
 const routes: Routes = [
   {path:"", redirectTo: "login", pathMatch: "full"},
   {path:"login", component: LoginComponent},
-  {path: "home", component: HomeComponent,
+  {path: "home", component: HomeComponent, canActivateChild: [GuardaHomeGuard], 
   children: [
     {path: "cadastrar", component: CadastrarComponent},
     {path: "faq", component: FaqComponent},
