51de4f7858c3f79ba926c6abe752b2e5e69a9e93
Camila Zelenovsky <camila.zelenovsky@cat.com>
2023-03-15 14:10:53 -0300

    Salvando

--- modulo1/semana10/src/app/components/home/home.component.ts
+++ modulo1/semana10/src/app/components/home/home.component.ts
@@ -10,7 +10,10 @@ export class HomeComponent {
   public produtos = [
     {id: "1", nome: "mesa", quantidade: "3", preco: "R$ 1.200,00"},
     {id: "2", nome: "cadeira", quantidade: "6", preco: "R$ 800,00"},
-    {id: "3", nome: "cama", quantidade: "3", preco: "R$ 2,500,00"},
+    {id: "3", nome: "cama", quantidade: "3", preco: "R$ 2.500,00"},
   ]
-  
+
+  public limparLocal () {
+    localStorage.clear();
+  }
 }
