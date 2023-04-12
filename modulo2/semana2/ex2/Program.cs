57839c2ed00e035e7d32ec2ded5edaab279bb259
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-12 19:45:56 -0300

    ex2

--- modulo2/semana2/ex2/Program.cs
+++ modulo2/semana2/ex2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");  
+        int numero = Convert.ToInt16(Console.ReadLine());
+
+        if(numero%2==0) {
+            Console.WriteLine("Esse número é par");
+        } else {
+           Console.WriteLine("Esse número é ímpar"); 
+        }
+    }
+}
