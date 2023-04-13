29fbd3edf563ce1733208220e5b6415c8ac7aa83
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-13 16:20:24 -0300

    ex3

--- modulo2/semana2/ex3/Program.cs
+++ modulo2/semana2/ex3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite seu nome: ");  
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite sua idade: ");  
+        int idade = Convert.ToInt16(Console.ReadLine());
+
+        while(idade<0) {
+            Console.Write("Idade inválida. Por favor, digite sua idade novamente: ");  
+            idade = Convert.ToInt16(Console.ReadLine());
+        }
+
+        if (idade>=0 & idade<18) {
+            Console.WriteLine($"{nome}, você é menor de idade.");
+        } else if(idade>=18 & idade<60) {
+             Console.WriteLine($"{nome}, você é maior de idade.");
+        } else if(idade>=60) {
+             Console.WriteLine($"{nome}, você é idoso.");
+        } else {
+            Console.WriteLine("Idade inválida");
+        }
+    }
+}
