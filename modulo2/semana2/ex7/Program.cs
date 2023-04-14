d048a55db5c4f3a3d3710cd4e0818e720c7ca266
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-13 21:26:33 -0300

    ex7

--- modulo2/semana2/ex7/Program.cs
+++ modulo2/semana2/ex7/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        int numero;
+
+        do
+        {
+        Console.Write("Digite um número inteiro: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+            if (numero <= 0) {
+                Console.Write("Número inválido, por favor, digite um número válido: ");
+            }
+        } while (numero <= 0); 
+
+        for(int i=0; i<=numero; i+=2) {
+            Console.Write($"{i} ");
+        }  
+        
+    }
+}
