743f49af1294c7e4d1eb13bd732fe676ad04aed8
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-17 20:01:09 -0300

    criação pastas exercícios

--- modulo2/semana2/ex9/Program.cs
+++ modulo2/semana2/ex9/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Qual a quantidade de números que você deseja fazer a média: ");  
+        int quantidade = Convert.ToInt32(Console.ReadLine());
+        int soma = 0;
+        double media;
+
+        int[] numeros = new int[quantidade];
+
+        for(int i=0; i<numeros.Length; i++) {
+            Console.Write("Digite o número desejado: ");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        foreach (var item in numeros)
+        {
+            soma += item;
+        }
+
+        Console.Write(media = soma / quantidade);
+
+    }
+}
