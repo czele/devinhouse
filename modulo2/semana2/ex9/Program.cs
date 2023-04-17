39a9920df0b18ce874c16564b90857d2a9f31544
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-17 20:04:10 -0300

    resolução de conflitos commit

--- modulo2/semana2/ex9/Program.cs
+++ modulo2/semana2/ex9/Program.cs
@@ -2,26 +2,6 @@
 {
     private static void Main(string[] args)
     {
-<<<<<<< HEAD
-        Console.Write("Qual a quantidade de números que você deseja fazer a média: ");  
-        int quantidade = Convert.ToInt32(Console.ReadLine());
-        int soma = 0;
-        double media;
-
-        int[] numeros = new int[quantidade];
-
-        for(int i=0; i<numeros.Length; i++) {
-            Console.Write("Digite o número desejado: ");
-            numeros[i] = Convert.ToInt32(Console.ReadLine());
-        }
-
-        foreach (var item in numeros)
-        {
-            soma += item;
-        }
-
-        Console.Write(media = soma / quantidade);
-=======
         Console.Write("Qual a quantidade de números que deseja calcular a média: ");
         int quantidade = Convert.ToInt32(Console.ReadLine());
         int[] numeros = new int[quantidade];
@@ -40,7 +20,6 @@
         //Console.WriteLine(soma);
 
         Console.WriteLine(soma/quantidade);
->>>>>>> 0279a0a6a1d87c7403ed824ee09f7419d79702b7
 
     }
 }
