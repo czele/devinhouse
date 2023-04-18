48133e6ef17ec174c07da9cf2d6dd43a3540c49f
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-17 21:45:33 -0300

    ex1

--- modulo2/semana3/exsoma/Program.cs
+++ modulo2/semana3/exsoma/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Calcular(num1, num2);
+    }
+
+    static double Calcular(double num1, double num2) {
+        double resultado = num1 + num2;
+        Console.WriteLine(resultado);
+        return resultado;
+    }
+
+}
