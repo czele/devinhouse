9a9e3fc771201dac8c4f5d54cf4e2cb45809e86b
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-18 20:16:49 -0300

    ex4

--- modulo2/semana3/excarros/Program.cs
+++ modulo2/semana3/excarros/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Tem que ser o nome do projeto para pegar as classes
+using excarros;
+
+Proprietario proprietario1 = new Proprietario(){
+    nome="João",
+    cpf=79240792031,
+    telefone=986658961
+};
+
+Proprietario proprietario2 = new Proprietario(){
+    nome="Maria",
+    cpf=04742412036,
+    telefone=979425013
+};
+
+Proprietario proprietario3 = new Proprietario(){
+    nome="Camila",
+    cpf=94420517093,
+    telefone=980686455
+};
---
+++ modulo2/semana3/excarros/Proprietario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Prop + tab cria o get e set
+namespace excarros
+{
+    public class Proprietario
+    {
+        public string nome {get; set;}
+        public long cpf {get; set;}
+        public long telefone {get; set;}
+    }
+}
