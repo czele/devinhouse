744d66f0ab28d731fd48470fa8787f6743988c2b
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-17 21:54:32 -0300

    ex2

--- modulo2/semana3/exsoma/Program.cs
+++ modulo2/semana3/exsoma/Program.cs
@@ -4,16 +4,33 @@
     {
 
         Console.WriteLine("Digite o primeiro número: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
+        int num1 = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("Digite o segundo número: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o terceiro número: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
 
         Calcular(num1, num2);
+        Calcular(num1, num2, num3);
     }
 
+
+    static int Calcular(int num1, int num2) {
+        int resultado = num1 + num2;
+        Console.WriteLine(resultado);
+        return resultado;
+    }
     static double Calcular(double num1, double num2) {
         double resultado = num1 + num2;
+        Console.WriteLine(resultado);
+        return resultado;
+    }
+
+        static double Calcular(double num1, double num2, double num3) {
+        double resultado = num1 + num2 + num3;
         Console.WriteLine(resultado);
         return resultado;
     }
