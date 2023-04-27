f0f2ac51d414115b329fa8301094b40cf93415c2
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-27 19:21:35 -0300

    ex1

--- modulo2/semana4/ex1/Pessoa.cs
+++ modulo2/semana4/ex1/Pessoa.cs
@@ -5,17 +5,11 @@ using System.Threading.Tasks;
 
 namespace ex1
 {
-    public class Pessoa
-    {
-<<<<<<< HEAD
+    public class Pessoa {
+
         private String _nome;
         protected int idade { get; set; }
         public String endereco { get; set; }
-=======
-        private String Nome;
-        protected int Idade { get; set; }
-        public String Endereco { get; set; }
->>>>>>> 2c372b17c8ebdc3d065fa7a46200a84d82de7aeb
 
         public Pessoa()
         {
@@ -24,23 +18,15 @@ namespace ex1
 
         public Pessoa(String nome, int idade)
         {
-<<<<<<< HEAD
             _nome = nome;
             this.idade = idade;
         }
 
         public void saudacao() {
             Console.WriteLine($"Olá, meu nome é {_nome} e eu tenho {idade} anos");
-=======
-            Nome = nome;
-            Idade = idade;
+
         }
 
-        public void saudacao() {
-            Console.WriteLine($"Olá, meu nome é {Nome} e eu tenho {Idade} anos");
->>>>>>> 2c372b17c8ebdc3d065fa7a46200a84d82de7aeb
-        }
-  
     }
 
 }
