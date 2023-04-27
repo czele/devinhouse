e8e31eccd01108ba749f6b04ec88caf775573e66
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-27 19:52:35 -0300

    ex2

--- modulo2/semana4/ex2/Program.cs
+++ modulo2/semana4/ex2/Program.cs
@@ -4,10 +4,10 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        ContaBancaria conta1 = new ContaBancaria("123", 200);
+        ContaBancaria conta1 = new ContaBancaria("123", 400);
 
-        // conta1.deposita(200);
-        // conta1.saca(500);
+        conta1.deposita(200);
+        conta1.saca(100);
         Console.WriteLine(conta1.getSaldo());
     }
 }
