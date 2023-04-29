0219e426068e598d3038f188b58abac27996dc7b
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-28 21:31:16 -0300

    ex7

--- modulo2/semana4/ex7/ContaBancaria.cs
+++ modulo2/semana4/ex7/ContaBancaria.cs
@@ -7,6 +7,31 @@ namespace ex7
 {
     public class ContaBancaria
     {
-        
+        public String NumeroConta { get; set; }
+        public const decimal Saldo = 1000;
+
+        public ContaBancaria() { }
+
+        public ContaBancaria(String numeroConta)
+        {
+            this.NumeroConta = NumeroConta;    
+        }
+
+        public void Depositar(decimal valor) {
+            if(valor > 0 && valor < Saldo) {
+                System.Console.WriteLine("Depósito permitido");
+            } else {
+                System.Console.WriteLine("Depósito negado");
+            }
+        }
+
+        public void Sacar(decimal valor) {
+            if(valor < Saldo) {
+                System.Console.WriteLine("Saque permitido");
+            } else {
+                System.Console.WriteLine("Saque negado");
+            }
+
+        }
     }
 }
--- modulo2/semana4/ex7/Program.cs
+++ modulo2/semana4/ex7/Program.cs
@@ -4,6 +4,8 @@ internal partial class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ContaBancaria conta1 = new ContaBancaria("123");
+        conta1.Depositar(10000);
+        conta1.Sacar(200);
     }
 }
