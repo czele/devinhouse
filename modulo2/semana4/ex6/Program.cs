32b3c296dd03a6bc302e38153fd4e961f6eb68d4
Camila Zelenovsky <camila_zele@hotmail.com>
2023-04-28 21:49:52 -0300

    ex6

--- modulo2/semana4/ex6/Program.cs
+++ modulo2/semana4/ex6/Program.cs
@@ -5,6 +5,8 @@ internal partial class Program
     private static void Main(string[] args)
     {
         Retangulo retangulo1 = new Retangulo(10, 20);
+        retangulo1.Altura = 10;
+        retangulo1.Largura = 20;
         retangulo1.CalcularArea();
 
         Retangulo retangulo2 = new Retangulo(-10, 20);
