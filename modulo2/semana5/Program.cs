5f05b2f022c25eff20fa3bba0f5c03963dd08a9a
Camila Zelenovsky <camila_zele@hotmail.com>
2023-05-06 17:11:47 -0300

    ex10

--- modulo2/semana5/Program.cs
+++ modulo2/semana5/Program.cs
@@ -5,10 +5,45 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //PessoaFisica pessoaFisica = new PessoaFisica(1, "Rua", "123", "Camila", "123", new DateTime(1990,05,1));
-        IClienteService clienteService = new ClienteService();
-        clienteService.CriarConta();
-        clienteService.ExibirClientes();
-        
-    }
+        String opcao;
+
+        do {
+            Console.WriteLine("\n\nBem Vindo ao banco Fullstack, Escolha uma opção para continuar: ");
+            Console.WriteLine("1 - Abrir Conta");
+            Console.WriteLine("2 - Consultar Conta");
+            Console.WriteLine("3 - Listar Todas as contas");
+            Console.WriteLine("4 - Sair\n");
+            opcao = Console.ReadLine();
+            SelecaoMenu(opcao);
+        } while (opcao != "4");       
+    
+
+        void SelecaoMenu(String opcao) {
+            IClienteService clienteService = new ClienteService();
+            switch(opcao) {
+            case "1":
+                clienteService.CriarConta();
+                break;
+            case "2":
+                Console.Write("Digite o número da conta: ");
+                int numeroConta = Int32.Parse(Console.ReadLine());
+                var cliente = clienteService.BuscarClientePorNumeroDeConta(numeroConta);
+                if(cliente == null) 
+                    Console.WriteLine("Conta não foi indentificada");
+                else
+                    Console.WriteLine(cliente.ResumoCliente());
+                break;
+            case "3":
+                clienteService.ExibirClientes();
+                break;
+            case "4":
+                break;
+            default:
+                Console.WriteLine("Opção inválida");
+                break;
+            }    
+
+        }
+    }    
+    
 }
