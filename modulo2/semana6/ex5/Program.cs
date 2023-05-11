99122d35862085994d043f02d1fbd1abfb8f428f
Camila Zelenovsky <camila_zele@hotmail.com>
2023-05-10 21:07:34 -0300

    ex5

---
+++ modulo2/semana6/ex5/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<double> lista = new List<double>(){ 1.5, 2.7, 3.2, 5 };  
+        List<double> listaNova = new List<double>();
+
+        for(int i=lista.Count()-1; i >= 0; i--) {
+            listaNova.Add(lista[i]);
+        }
+        
+        foreach(var item in listaNova) {
+            Console.WriteLine(item);
+        }
+
+    }
+}
