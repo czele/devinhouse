7d41eb069504e8fa7a98714bb275c5ac9eda6082
Camila Zelenovsky <camila_zele@hotmail.com>
2023-05-12 19:55:25 -0300

    ex4

---
+++ modulo2/semana6/ex1/exercicio1.Teste/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace exercicio1.Teste;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
