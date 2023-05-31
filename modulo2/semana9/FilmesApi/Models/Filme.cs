c2caf6b02a8bad2c5ff026fd2931c082b4134318
Camila Zelenovsky <camila_zele@hotmail.com>
2023-05-30 21:26:59 -0300

    data anotation

--- modulo2/semana9/FilmesApi/Models/Filme.cs
+++ modulo2/semana9/FilmesApi/Models/Filme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,17 @@ namespace FilmesApi.Models
     public class Filme
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="O campo nome é de preenchimento obrigatório")]
+        [MaxLength(200, ErrorMessage = "O campo nome não pode exceder 200 caracteres")]
 
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "O campo diretor é de preenchimento obrigatório")]
         public string Diretor { get; set; }
 
+        [Required(ErrorMessage = "O campo genero é de preenchimento obrigatório")]
         public string Genero { get; set; }
+
 
         public int Duracao { get; set; }
     }
