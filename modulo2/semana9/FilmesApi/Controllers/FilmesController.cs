4bfefef94d2cb1abdbf1ef703bc5e7b0bb35c4a8
Camila Zelenovsky <camila_zele@hotmail.com>
2023-05-30 21:22:58 -0300

    filmes

---
+++ modulo2/semana9/FilmesApi/Controllers/FilmesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmesApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FilmesApi.Controllers
+{
+    [Route("api/filmes")]
+    [ApiController]
+    public class FilmesController : ControllerBase
+    {
+        // GET: api/<FilmesController>
+        [HttpGet]
+        public IEnumerable<Filme> Get()
+        {
+            return MockFilmes.Filmes;
+        }
+
+        // GET api/<FilmesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FilmesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FilmesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FilmesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
