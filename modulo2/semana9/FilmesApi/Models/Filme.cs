f7ecf634de23dc4a20cec0118950b7bd8713e4c7
Camila Zelenovsky <camila_zele@hotmail.com>
2023-06-05 20:07:38 -0300

    modelo para o EF

--- modulo2/semana9/FilmesApi/Models/Filme.cs
+++ modulo2/semana9/FilmesApi/Models/Filme.cs
@@ -8,6 +8,7 @@ namespace FilmesApi.Models
 {
     public class Filme
     {
+        [Key]
         public int Id { get; set; }
         [Required(ErrorMessage ="O campo nome é de preenchimento obrigatório")]
         [MaxLength(200, ErrorMessage = "O campo nome não pode exceder 200 caracteres")]
@@ -15,12 +16,14 @@ namespace FilmesApi.Models
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo diretor é de preenchimento obrigatório")]
+        [MaxLength(100, ErrorMessage = "O campo diretor não pode exceder 100 caracteres")]
         public string Diretor { get; set; }
 
         [Required(ErrorMessage = "O campo genero é de preenchimento obrigatório")]
+        [MaxLength(50, ErrorMessage = "O campo gênero não pode exceder 50 caracteres")]
         public string Genero { get; set; }
 
-
+        [Required]
         public int Duracao { get; set; }
     }
 }
