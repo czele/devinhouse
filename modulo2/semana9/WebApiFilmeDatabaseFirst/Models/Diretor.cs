b8b36ddd9908c605dee3d5d99dbb239d88bd9c20
Camila Zelenovsky <camila_zele@hotmail.com>
2023-06-07 21:26:46 -0300

    model diretor

---
+++ modulo2/semana9/WebApiFilmeDatabaseFirst/Models/Diretor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiFilmeDatabaseFirst.Models
+{
+    public class Diretor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(150, ErrorMessage ="Este campo aceita até 150 caracteres")]
+        [MinLength(3, ErrorMessage = "Deve ter no mínimo 3 caracteres")]
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
