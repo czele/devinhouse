30616c33a4215fd2ab03aedd624d18eb80b1fad2
Camila Zelenovsky <camila_zele@hotmail.com>
2023-06-08 17:07:13 -0300

    migations Filme Diretor

---
+++ modulo2/semana9/WebApiFilmeDatabaseFirst/Migrations/20230608200605_CricacaoTabelasDiretorFilmeDiretores.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiFilmeDatabaseFirst.Migrations
+{
+    public partial class CricacaoTabelasDiretorFilmeDiretores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diretores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diretores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Diretor = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Genero = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Duracao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FilmeDiretores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdFilme = table.Column<int>(type: "int", nullable: false),
+                    IdDiretor = table.Column<int>(type: "int", nullable: false),
+                    DiretoresId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilmeDiretores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FilmeDiretores_Diretores_DiretoresId",
+                        column: x => x.DiretoresId,
+                        principalTable: "Diretores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FilmeDiretores_Filmes_IdFilme",
+                        column: x => x.IdFilme,
+                        principalTable: "Filmes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FilmeDiretores_DiretoresId",
+                table: "FilmeDiretores",
+                column: "DiretoresId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FilmeDiretores_IdFilme",
+                table: "FilmeDiretores",
+                column: "IdFilme");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FilmeDiretores");
+
+            migrationBuilder.DropTable(
+                name: "Diretores");
+
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
